00072f834e64cf3e7ab708515a95c5ef085785d0
Chad <chadjcampbell@gmail.com>
2023-12-25 11:41:51 -0500

    add city target select menu

--- global_thermonuclear_war.py
+++ global_thermonuclear_war.py
@@ -1,7 +1,7 @@
 import os
 
 from simple_term_menu import TerminalMenu
-from war_maps import starter_map, select_usa
+from war_maps import starter_map, select_usa, select_russia
 
 
 def global_thermonuclear_war():
@@ -12,6 +12,10 @@ def global_thermonuclear_war():
     if country == 'USA':
         os.system('clear')
         print(select_usa)
+    if country == 'Russia':
+        os.system('clear')
+        print(select_russia)
+    select_target(country)
 
 
 # helper functions
@@ -19,6 +23,22 @@ def select_country():
     print('SELECT YOUR COUNTRY')
     print('  Move with Up/Down, Enter to Select\n')
     options = ["USA", "Russia"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    return options[menu_entry_index]
+
+
+usa_cities = ["New York", "Washington DC", "Los Angeles"]
+russia_cities = ["Moscow", "Leningrad", "Kyev"]
+
+
+def select_target(country):
+    print('SELECT YOUR TARGET')
+    print('  Move with Up/Down, Enter to Select\n')
+    if country == 'USA':
+        options = russia_cities
+    if country == 'Russia':
+        options = usa_cities
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
     return options[menu_entry_index]
